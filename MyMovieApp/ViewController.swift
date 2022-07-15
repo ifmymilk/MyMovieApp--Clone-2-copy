1bca1b40197650f7fb04e95e0898678289849c6c
ifmymilk <59724526+ifmymilk@users.noreply.github.com>
2022-07-15 12:34:22 +0200

    Initial Commit

---
+++ MyMovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMovieApp
+//
+//  Created by Simon LE on 15/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
